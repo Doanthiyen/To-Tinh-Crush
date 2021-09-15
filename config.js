8a52ba9fe8b3ed85e98d293ed978b7801b341700
Doanthiyen <86780648+Doanthiyen@users.noreply.github.com>
2021-09-15 16:43:23 +0700

    Em yêu anhhh:3

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    introTitle: 'Vũ Hoàng Anh <3',
+    introDesc: `Là em đây bbi`,
+    btnIntro: 'xin chào em bé của tui',
+    title: 'Em có điều này muốn hỏi anh nhớ trả lời thật lòng nha:33',
+    desc: 'Do you really love me',
+    btnYes: 'Tất nhiên rồi bbi',
+    btnNo: 'Không',
+    question:'Will you marry me <3',
+    btnReply: 'Gửi cho bé <3',
+    reply: ' em sợ mất anh lắm :((',
+    mess: ' anh sẽ không rời đi đâu:33',
+    messDesc: 'em yêu anh rất nhiều',
+    btnAccept: ' hãy nhắn tin cho em nhó:3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
